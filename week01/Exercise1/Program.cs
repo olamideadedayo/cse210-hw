1526ab1025264c72676e297d56dafae3c0ae2297
olamideadedayo <olamideadedayo001@gmail.com>
2025-05-11 16:10:21 -0600

    CSE wk 1 assignment

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -6,4 +6,15 @@ class Program
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
     }
+    static void GreetUser()
+    {
+        Console.WriteLine("Hello! Welcome to the program.");
+        Console.WriteLine("what isyour first name?");
+        string first = Console.ReadLine();
+         Console.WriteLine("what isyour last name?");
+         string last = Console.ReadLine();
+          Console.WriteLine($"Your name is {last}, {first} {last}.");
+    }
+    
+    
 }
